e20884dc67ecf56174a1723cd9fdbc737264067a
lfairclo <137823676+lfairclo@users.noreply.github.com>
2025-05-24 13:18:49 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const socket = io('https://your-backend.onrender.com'); // replace with your backend URL
+const socket = io('https://house-point-system-arnold-house.onrender.com'); // replace with your backend URL
 const valueSpan = document.getElementById('value');
 
 socket.on('update', (newValue) => {
