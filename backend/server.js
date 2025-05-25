b558d043ee74aa9d83dda00268173c974b7394c4
lfairclo <137823676+lfairclo@users.noreply.github.com>
2025-05-25 17:02:36 +0100

    Revert server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const http = require('http');
+const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server, {
-    cors: {
-        origin: '*',
-    },
-});
-
-app.use(cors());
+@@ -15,19 +14,6 @@ app.use(cors());
 
 let sharedInt = 0;
 
+io.on('connection', (socket) => {
+    socket.emit('update', sharedInt);
+
+    socket.on('increment', () => {
+        sharedInt++;
+        io.emit('update', sharedInt);
+    });
+
+    socket.on('decrement', () => {
+        sharedInt--;
+        io.emit('update', sharedInt);
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
