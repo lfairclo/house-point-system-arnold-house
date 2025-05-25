89caa120145061e28e0ec1f30d96a017cbf9a4e5
lfairclo <137823676+lfairclo@users.noreply.github.com>
2025-05-25 17:00:21 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const http = require('http');
-const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
@@ -15,19 +14,6 @@ app.use(cors());
 
 let sharedInt = 0;
 
-io.on('connection', (socket) => {
-    socket.emit('update', sharedInt);
-
-    socket.on('increment', () => {
-        sharedInt++;
-        io.emit('update', sharedInt);
-    });
-
-    socket.on('decrement', () => {
-        sharedInt--;
-        io.emit('update', sharedInt);
-    });
-});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
