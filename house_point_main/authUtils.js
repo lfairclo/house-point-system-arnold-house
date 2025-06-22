287bd6668b642fb8299133708e15e77d50d997f8
lfairclo <137823676+lfairclo@users.noreply.github.com>
2025-06-22 13:44:28 +0100

    add app check

--- house_point_main/authUtils.js
+++ house_point_main/authUtils.js
@@ -1,6 +1,14 @@
 // authUtils.js
 
 import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app-check.js";
+
+const app = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6LfJBmgrAAAAAOXs8gR2yQf6Hw34cmAkY_gBizP0'),
+  isTokenAutoRefreshEnabled: true
+});
 
 /**
  * Gets the role of a user based on your teacher/student structure.
